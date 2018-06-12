947a0212a41ba3a73857194f98cdcd39b27d5dd1
pig <jialihuan265@163.com>
2018-06-11 23:02:13 -0700

    05

---
+++ decoratorPattern_05/question02.py
@@ -0,0 +1,21 @@
+
+def withpara(para=True):
+    def inner(fn):
+        if para ==False:
+            return fn
+
+        def wrap_in(*args):
+            print('inner start')
+            fn(*args)
+            print('inner end')
+
+        return wrap_in
+
+    return inner
+
+@withpara(False)
+def f(a):
+    return a+2
+
+if __name__=='__main__':
+    print(f(5))
