ce69dd1e61888f7d4f7640c36a57bb4f9bcdcafe
pig <jialihuan265@163.com>
2018-06-20 02:57:25 -0700

    proxy

---
+++ proxyPattern_09/lazy.py
@@ -0,0 +1,37 @@
+#coding : utf-8
+class LazyProperty:
+    def __init__(self,method):
+        self.method=method
+        self.method_name=method.__name__
+        #print('function overriden: {}'.format(self.fget))
+        #print("function's name: {}".format(self.func_name))
+    def __get__(self, instance, owner):
+        if not instance:
+            return  None
+        value=self.method(instance)
+        #print(('value {}'.format(value)))
+        setattr(instance,self.method_name,value)
+        return value
+
+class Test:
+    def __init__(self):
+        self.x='foo'
+        self.y='bar'
+        self._resouce=None
+
+    @LazyProperty
+    def resource(self):
+        print('initializing self._resource which is: {}'.format(self._resouce))
+        self._resouce=tuple(range(5))  # 代价大的
+        return self._resouce
+
+def main():
+    t=Test()
+    print(t.x)
+    print(t.y)
+    # 做更多的事情。。。
+    print(t.resource)
+    print(t.resource)
+
+if __name__ == '__main__':
+    main()
