4a16fa28f429bf6b893a92e02af1dc0acb61df74
pig <jialihuan265@163.com>
2018-09-03 00:43:00 -0700

    factory pattern

--- factoryPattern_01/factory_method.py
+++ factoryPattern_01/factory_method.py
@@ -1,60 +1,67 @@
 import xml.etree.ElementTree as etree
 import json
 
+
 class JSONConnector:
-    def __init__(self,filepath):
-        self.data=dict()
-        with open(filepath,mode='r',encoding='utf-8') as f:
-            self.data=json.load(f)
+    def __init__(self, filepath):
+        self.data = dict()
+        with open(filepath, mode='r', encoding='utf-8') as f:
+            self.data = json.load(f)
+
     @property
     def parsed_data(self):
         return self.data
 
+
 class XMLConnector:
-    def __init__(self,filepath):
-        self.tree=etree.parse(filepath)
+    def __init__(self, filepath):
+        self.tree = etree.parse(filepath)
 
     @property
     def parsed_data(self):
         return self.tree
 
+
 def connector_factory(filepath):
     if filepath.endswith('json'):
-        connector=JSONConnector
+        connector = JSONConnector
     elif filepath.endswith('xml'):
-        connector=XMLConnector
+        connector = XMLConnector
     else:
         raise ValueError('Cannot connect to {}'.format(filepath))
     return connector(filepath)
 
+
 def connect_to(filepath):
-    factory=None
+    factory = None
     try:
-        factory=connector_factory(filepath)
+        factory = connector_factory(filepath)
     except ValueError as ve:
         print(ve)
     return factory
 
-def main():
-    sqlite_factory=connect_to('data/person.sq3')
 
-    xml_factory=connect_to('data/person.xml')
-    xml_data=xml_factory.parsed_data
-    liars=xml_data.findall(".//{}[{}='{}']".format('person','lastName','Liar'))
+def main():
+    sqlite_factory = connect_to('data/person.sq3')
+
+    xml_factory = connect_to('data/person.xml')
+    xml_data = xml_factory.parsed_data
+    liars = xml_data.findall(".//{}[{}='{}']".format('person', 'lastName', 'Liar'))
     print('found: {} persons'.format(len(liars)))
     for liar in liars:
         print('first name: {}'.format(liar.find('firstName').text))
         print('last name: {}'.format(liar.find('lastName').text))
-        [print('phone number: ({}) {}'.format(p.attrib['type'],p.text))
+        [print('phone number: ({}) {}'.format(p.attrib['type'], p.text))
          for p in liar.find('phoneNumbers')]
 
-    json_factory=connect_to('data/donut.json')
-    json_data=json_factory.parsed_data
+    json_factory = connect_to('data/donut.json')
+    json_data = json_factory.parsed_data
     print('found: {} dnouts'.format(len(json_data)))
-    for donut in  json_data:
+    for donut in json_data:
         print('name: {}'.format(donut['name']))
         print('price: ${}'.format(donut['ppu']))
-        [print('topings: \t{}\t{}'.format(t['id'],t['type']))
+        [print('topings: \t{}\t{}'.format(t['id'], t['type']))
          for t in donut['topping']]
+
 
 main()
