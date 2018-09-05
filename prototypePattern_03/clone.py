e0c9c6c3ece848fb4af72018f4d3a85d2a0263d1
pig <jialihuan265@163.com>
2018-09-04 21:00:51 -0700

    prototype pattern

--- prototypePattern_03/clone.py
+++ prototypePattern_03/clone.py
@@ -21,3 +21,6 @@ if __name__ == '__main__':
     c = copy.deepcopy(b)
     print([str(i) for i in (b, c)])
     print([(i, i.A) for i in (b, c)])
+#Output:
+# ['18, Hello, 34', '18, Hello, 34']
+# [(<__main__.B object at 0x7f0dc45c0710>, <__main__.A object at 0x7f0dc45f9588>), (<__main__.B object at 0x7f0dc465efd0>, <__main__.A object at 0x7f0dc45f9588>)]
