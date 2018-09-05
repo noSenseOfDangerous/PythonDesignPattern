82952012fd1155ec72cb71ed9679b9eb3e63a7c5
pig <jialihuan265@163.com>
2018-09-04 20:53:05 -0700

    builder pattern

---
+++ prototypePattern_03/clone.py
@@ -0,0 +1,16 @@
+import copy
+class A:
+    def __init__(self):
+        self.x=18
+        self.msg='Hello'
+class B():
+    def __init__(self):
+        self.A=A()
+        self.y=34
+    def __str__(self):
+        return '{}, {}, {}'.format(self.A.x,self.A.msg,self.y)
+if __name__ == '__main__':
+    b=B()
+    c=copy.deepcopy(b)
+    print([str(i) for i in (b,c)])
+    print([(i,i.A )for i in (b,c)])
